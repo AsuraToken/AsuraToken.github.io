c0f105bc408c1fb5ffd940b967268e2426bca391
namewzz <namewzz@foxmail.com>
2021-06-28 17:21:16 +0800

    fix transfer fail

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "b2b1084d2438d1e89242233c405ffcf1",
-"index.html": "b763ca1f00a16964e0601692fc42c016",
-"/": "b763ca1f00a16964e0601692fc42c016",
-"main.dart.js": "bb54ad6c06b883c48cc3552ec01df0e3",
+"index.html": "34049229f88dc3a26b0afcfede82bd7b",
+"/": "34049229f88dc3a26b0afcfede82bd7b",
+"main.dart.js": "402551b8bfdd9eb58ad754516e02b0df",
 "favicon.png": "83f04be251df05ceb50f605cd04b6896",
 "icons/Icon-192.png": "a0b9a8338d075daaddab06e5e3a201a2",
 "icons/Icon-512.png": "4eb7d384eaeb10921e1d9ef689282ed4",
